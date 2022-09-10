3c4918bdbb8b42ccbba541e9f66d00d08ff0098c
Kesavaram Krishnamoorthy <kesavaram994@gmail.com>
2022-09-10 16:17:37 +0530

    refined output and added display offset variables

--- profit calculator application.py
+++ profit calculator application.py
@@ -19,49 +19,52 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        self.xPosPLOffset = 100
+        self.yPosPLOffset = 100
+
         #create a label
         self.titleLabel = QLabel(self)
         self.titleLabel.setText("P&L calculator")
-        self.titleLabel.move(100,10)
+        self.titleLabel.move(self.xPosPLOffset + 100,self.yPosPLOffset + 10)
         self.titleLabel.setLineWidth(100)
 
         #create Buy label
         self.buyLabel = QLabel(self)
         self.buyLabel.setText("buy Price")
-        self.buyLabel.move(10,60)
+        self.buyLabel.move(self.xPosPLOffset + 10,self.yPosPLOffset + 60 )
         self.buyLabel.setLineWidth(100)
 
 
         # Create buy Price  textbox
         self.buyPriceBox = QLineEdit(self)
         self.buyPriceBox.windowTitle = "buy"
-        self.buyPriceBox.move(100, 60)
+        self.buyPriceBox.move(self.xPosPLOffset + 100, self.yPosPLOffset + 60)
         self.buyPriceBox.resize(100,40)
 
 
          # Create sell Price  textbox
         self.sellPriceBox = QLineEdit(self)
         self.sellPriceBox.windowTitle = "sell"
-        self.sellPriceBox.move(100, 120)
+        self.sellPriceBox.move(self.xPosPLOffset + 100, self.yPosPLOffset + 120)
         self.sellPriceBox.resize(100,40)
 
         #create Buy label
         self.sellPricelabel = QLabel(self)
         self.sellPricelabel.setText("sell Price")
-        self.sellPricelabel.move(10,120)
+        self.sellPricelabel.move(self.xPosPLOffset + 10,self.yPosPLOffset + 120)
         self.sellPricelabel.setLineWidth(100)
 
 
          #create Result label
         self.Resultlabel = QLabel(self)
         self.Resultlabel.setText("Percent:")
-        self.Resultlabel.move(10,250)
+        self.Resultlabel.move(self.xPosPLOffset + 10,self.yPosPLOffset + 250)
         self.Resultlabel.setLineWidth(100)
 
         #create Result percent label
         self.ResultPercentlabel = QLabel(self)
         self.ResultPercentlabel.setText("")
-        self.ResultPercentlabel.move(90,250)
+        self.ResultPercentlabel.move(self.xPosPLOffset + 90,self.yPosPLOffset + 250)
         self.ResultPercentlabel.setLineWidth(100)
 
 
@@ -72,7 +75,7 @@ class App(QMainWindow):
         self.button = QPushButton('Calculate', self)
         self.button.setDefault = True
         self.button.setAutoDefault = False
-        self.button.move(20,200)
+        self.button.move(self.xPosPLOffset + 20,self.yPosPLOffset + 200)
         
         # connect button to function on_click
        
@@ -83,9 +86,9 @@ class App(QMainWindow):
     def on_click(self):
         buyPrice = float(self.buyPriceBox.text()) 
         sellPrice = float(self.sellPriceBox.text())
-        PLpercent = ((sellPrice - buyPrice) / buyPrice )*100.00
+        PLpercent = ((sellPrice - buyPrice) / buyPrice )*100.00 
         diff = sellPrice - buyPrice
-        result = str(PLpercent)
+        result = f'{PLpercent:.2f}' #formats output to 2 decimal places.
         
         self.ResultPercentlabel.setText(str(result))
         
