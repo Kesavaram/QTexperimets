4739a37488aa9c99fae39043d78edc02b9f85c7a
Kesavaram Krishnamoorthy <kesavaram994@gmail.com>
2022-09-12 19:24:01 +0530

    Added UI boxes

--- Margin SL and TP price calculator.py
+++ Margin SL and TP price calculator.py
@@ -19,7 +19,7 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.xPosPLOffset = 225
+        self.xPosPLOffset = 150
         self.yPosPLOffset = 25
 
         #create a label
@@ -69,6 +69,10 @@ class App(QMainWindow):
         self.marginMultBox.resize(100,40)
 
 
+        #create Buy QTY label
+        
+
+
         #create fee percent label
         self.sellPricelabel = QLabel(self)
         self.sellPricelabel.setText("Fees percent")
@@ -84,25 +88,124 @@ class App(QMainWindow):
 
          #create Result label
         self.Resultlabel = QLabel(self)
-        self.Resultlabel.setText("Percent:")
-        self.Resultlabel.move(self.xPosPLOffset + 10,self.yPosPLOffset + 500)
+        self.Resultlabel.setText("PL Percent:")
+        self.Resultlabel.move(self.xPosPLOffset -30 ,self.yPosPLOffset + 500)
         self.Resultlabel.setLineWidth(100)
 
         #create Result percent label
         self.ResultPercentlabel = QLabel(self)
         self.ResultPercentlabel.setText("")
-        self.ResultPercentlabel.move(self.xPosPLOffset + 90,self.yPosPLOffset + 500)
+        self.ResultPercentlabel.move(self.xPosPLOffset +60,self.yPosPLOffset + 500)
         self.ResultPercentlabel.setLineWidth(100)
 
         # Create a button in the window
-        self.button = QPushButton('Calculate', self)
-        self.button.setDefault = True
-        self.button.setAutoDefault = False
-        self.button.move(self.xPosPLOffset + 20,self.yPosPLOffset + 400)
+        self.CalcPercentButton = QPushButton('Calculate PL percent', self)
+        self.CalcPercentButton.setDefault = True
+        self.CalcPercentButton.setAutoDefault = False
+        self.CalcPercentButton.adjustSize()
+        self.CalcPercentButton.move(self.xPosPLOffset - 40,self.yPosPLOffset + 400)
+
+
+        self.marginSLTPxPosOffset = self.xPosPLOffset + 300;
+        self.marginSLTPyPosOffset = self.yPosPLOffset;   
+
+        #create a label
+        self.MarginPriceCalcLabel = QLabel(self)
+        self.MarginPriceCalcLabel.setText("Margin SL & TP price calculator")
+        self.MarginPriceCalcLabel.adjustSize()
+        self.MarginPriceCalcLabel.move(self.marginSLTPxPosOffset ,self.marginSLTPyPosOffset + 15)
+        self.MarginPriceCalcLabel.setLineWidth(100)  
+
+        #create Buy price for SL TP label
+        self.buyLabelSLTP = QLabel(self)
+        self.buyLabelSLTP.setText("Buy Price ")
+        self.buyLabelSLTP.move(self.marginSLTPxPosOffset ,self.marginSLTPyPosOffset + 70 )
+        self.buyLabelSLTP.setLineWidth(100)
+        self.buyLabelSLTP.adjustSize()
+
+
+        # Create buy Price  textbox
+        self.buyPriceBoxSLTP = QLineEdit(self)
+        self.buyPriceBoxSLTP.windowTitle = "buy"
+        self.buyPriceBoxSLTP.move(self.marginSLTPxPosOffset  + 130, self.marginSLTPyPosOffset + 60)
+        self.buyPriceBoxSLTP.resize(100,40)
+
+
+         #create TP label
+        self.TP = QLabel(self)
+        self.TP.setText("TP USDT")
+        self.TP.move(self.marginSLTPxPosOffset ,self.marginSLTPyPosOffset + 130 )
+        self.TP.setLineWidth(100)
+ 
+        # Create TP  textbox
+        self.TPbox = QLineEdit(self)
+        self.TPbox.windowTitle = "TP USDT"
+        self.TPbox.move(self.marginSLTPxPosOffset  + 130, self.marginSLTPyPosOffset + 120)
+        self.TPbox.resize(100,40)
+
+        # create SL label
+        self.TP = QLabel(self)
+        self.TP.setText("SL USDT")
+        self.TP.move(self.marginSLTPxPosOffset ,self.marginSLTPyPosOffset + 180 )
+        self.TP.setLineWidth(100)
+ 
+        # Create TP  textbox
+        self.TPbox = QLineEdit(self)
+        self.TPbox.windowTitle = "SL USDT"
+        self.TPbox.move(self.marginSLTPxPosOffset  + 130, self.marginSLTPyPosOffset + 170)
+        self.TPbox.resize(100,40)
+
+
+
+        self.buyQTYLabel = QLabel(self)
+        self.buyQTYLabel.setText("Buy Qty(USDT)")
+        self.buyQTYLabel.move(self.marginSLTPxPosOffset ,self.marginSLTPyPosOffset + 230)
+        self.buyQTYLabel.adjustSize()
+        self.buyQTYLabel.setLineWidth(100)
+        #create Buy Qty box
+        self.TPpriceValue = QLabel(self)
+        self.TPpriceValue.setText("")
+        self.TPpriceValue.move(self.marginSLTPxPosOffset +60,self.marginSLTPyPosOffset + 500)
+        self.TPpriceValue.setLineWidth(100)
+
+        self.CalcPricesButton = QPushButton('Calculate prices for SL and TP', self)
+        self.CalcPricesButton.setDefault = True
+        self.CalcPricesButton.setAutoDefault = False
+        self.CalcPricesButton.adjustSize()
+        self.CalcPricesButton.move(self.marginSLTPxPosOffset - 10 ,self.marginSLTPyPosOffset + 400)
+
+        self.TPpriceLabel = QLabel(self)
+        self.TPpriceLabel.setText("Take profit price:")
+        self.TPpriceLabel.adjustSize()
+        self.TPpriceLabel.move(self.marginSLTPxPosOffset -15 ,self.marginSLTPyPosOffset + 460)
+        self.TPpriceLabel.setLineWidth(100)
+
+        #create Result percent label
+        self.TPpriceValue = QLabel(self)
+        self.TPpriceValue.setText("")
+        self.TPpriceValue.move(self.marginSLTPxPosOffset +60,self.marginSLTPyPosOffset + 460)
+        self.TPpriceValue.setLineWidth(100)
+
+        self.SLpriceLabel = QLabel(self)
+        self.SLpriceLabel.setText("Stop Loss price:")
+        self.SLpriceLabel.adjustSize()
+        self.SLpriceLabel.move(self.marginSLTPxPosOffset -15 ,self.marginSLTPyPosOffset + 500)
+        self.SLpriceLabel.setLineWidth(100)
+
         
+
+
+
+        #create  Margin Multiplier textbox
+        self.buyQtyBox = QLineEdit(self)
+        self.buyQtyBox.move(self.marginSLTPxPosOffset + 125, self.marginSLTPyPosOffset + 220)
+        self.buyQtyBox.resize(100,40)
+
+
+
+
         # connect button to function on_click
-       
-        self.button.pressed.connect(self.on_click)
+        self.CalcPercentButton.pressed.connect(self.on_click)
         self.show()
     
     @pyqtSlot()
